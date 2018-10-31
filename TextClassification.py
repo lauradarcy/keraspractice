4c8ad478fa1433d696f86db970d1fa43692fc075
luludarcy <luludarcy@gmail.com>
2018-10-31 11:00:22 +0000

    completed tutorial section

--- TextClassification.py
+++ TextClassification.py
@@ -69,4 +69,77 @@ model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation=tf.nn.relu))
 model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
+
+
+'''
+1. The first layer is an Embedding layer. This layer takes the integer-encoded vocabulary and looks up the embedding vector for each word-index. 
+	These vectors are learned as the model trains. The vectors add a dimension to the output array. The resulting dimensions are: (batch, sequence, embedding).
+
+2. Next, a GlobalAveragePooling1D layer returns a fixed-length output vector for each example by averaging over the sequence dimension. 
+	This allows the model to handle input of variable length, in the simplest way possible.
+
+3. This fixed-length output vector is piped through a fully-connected (Dense) layer with 16 hidden units.
+
+4. The last layer is densely connected with a single output node. 
+	Using the sigmoid activation function, this value is a float between 0 and 1, representing a probability, or confidence level.
+'''
+
 print(model.summary())
+#compile
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+#create a validation set to train on
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+#train the model!
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+history_dict = history.history
+print(history_dict.keys())
+
+#plot the training and validation loss for comparison, as well as the training and validation accuracy
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+fig = plt.gcf()
+fig.canvas.set_window_title('Training and Validation Loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.figure()
+fig = plt.gcf()
+fig.canvas.set_window_title('Training and Validation Accuracy')
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
