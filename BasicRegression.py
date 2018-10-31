0682ffd25106ee51ed128db632d8804bae871556
luludarcy <luludarcy@gmail.com>
2018-10-31 16:34:28 +0000

    completed

--- BasicRegression.py
+++ BasicRegression.py
@@ -5,4 +5,158 @@ from tensorflow import keras
 
 import numpy as np
 
-print(tf.__version__)
+'''
+In a regression problem, we aim to predict the output of a continuous value, like a price or a probability. 
+Contrast this with a classification problem, where we aim to predict a discrete label (for example, where a picture contains an apple or an orange).
+
+This notebook builds a model to predict the median price of homes in a Boston suburb during the mid-1970s. 
+To do this, we'll provide the model with some data points about the suburb, such as the crime rate and the local property tax rate.
+'''
+
+boston_housing = keras.datasets.boston_housing
+
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+
+# Shuffle the training set
+order = np.argsort(np.random.random(train_labels.shape))
+train_data = train_data[order]
+train_labels = train_labels[order]
+
+print("Training set: {}".format(train_data.shape))  # 404 examples, 13 features
+print("Testing set:  {}".format(test_data.shape))   # 102 examples, 13 features
+
+print(train_data[0])  # Display sample features, notice the different scales
+
+import pandas as pd
+
+'''
+The dataset contains 13 different features:
+
+CRIM 	- Per capita crime rate.
+ZN 		- The proportion of residential land zoned for lots over 25,000 square feet.
+INDUS 	- The proportion of non-retail business acres per town.
+CHAS 	- Charles River dummy variable (= 1 if tract bounds river; 0 otherwise).
+NOX 	- Nitric oxides concentration (parts per 10 million).
+RM 		- The average number of rooms per dwelling.
+AGE 	- The proportion of owner-occupied units built before 1940.
+DIS 	- Weighted distances to five Boston employment centers.
+RAD 	- Index of accessibility to radial highways.
+TAX 	- Full-value property-tax rate per $10,000.
+PTRATIO	- Pupil-teacher ratio by town.
+B 		- 1000 * (Bk - 0.63) ** 2 where Bk is the proportion of Black people by town.
+LSTAT 	- Percentage lower status of the population.
+'''
+
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+                'TAX', 'PTRATIO', 'B', 'LSTAT']
+
+df = pd.DataFrame(train_data, columns=column_names)
+print(df.head())
+
+#The labels are the house prices in thousands of dollars. (You may notice the mid-1970s prices.)
+print(train_labels[0:10])  # Display first 10 entries
+
+'''
+
+normalise features
+
+'''
+
+# Test data is *not* used when calculating the mean and std
+
+mean = train_data.mean(axis=0)
+std = train_data.std(axis=0)
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+
+print(train_data[0])  # First training sample, normalized
+
+#------create the model-----------
+
+def build_model():
+  model = keras.Sequential([
+    keras.layers.Dense(64, activation=tf.nn.relu,
+                       input_shape=(train_data.shape[1],)),
+    keras.layers.Dense(64, activation=tf.nn.relu),
+    keras.layers.Dense(1)
+  ])
+
+  optimizer = tf.train.RMSPropOptimizer(0.001)
+
+  model.compile(loss='mse',
+                optimizer=optimizer,
+                metrics=['mae'])
+  return model
+
+model = build_model()
+model.summary()
+
+#------train the model------------
+# Display training progress by printing a single dot for each completed epoch
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
+
+EPOCHS = 500
+
+# Store training stats
+history = model.fit(train_data, train_labels, epochs=EPOCHS,
+                    validation_split=0.2, verbose=0,
+                    callbacks=[PrintDot()])
+
+#visualise models
+import matplotlib.pyplot as plt
+
+print('\n',history.history.keys())
+
+def plot_history(history):
+  plt.figure()
+  plt.xlabel('Epoch')
+  plt.ylabel('Mean Abs Error [1000$]')
+  plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+           label='Train Loss')
+  plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+           label = 'Val loss')
+  plt.legend()
+  plt.ylim([0, 5])
+
+plot_history(history)
+
+model = build_model()
+
+# The patience parameter is the amount of epochs to check for improvement
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+
+history = model.fit(train_data, train_labels, epochs=EPOCHS,
+                    validation_split=0.2, verbose=0,
+                    callbacks=[early_stop, PrintDot()])
+
+
+
+plot_history(history)
+
+[loss, mae] = model.evaluate(test_data, test_labels, verbose=0)
+
+print("Testing set Mean Abs Error: ${:7.2f}".format(mae * 1000))
+
+#-------------predict-----------------
+
+plt.figure()
+test_predictions = model.predict(test_data).flatten()
+
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [1000$]')
+plt.ylabel('Predictions [1000$]')
+plt.axis('equal')
+plt.xlim(plt.xlim())
+plt.ylim(plt.ylim())
+_ = plt.plot([-100, 100], [-100, 100])
+
+plt.figure()
+error = test_predictions - test_labels
+plt.hist(error, bins = 50)
+plt.xlabel("Prediction Error [1000$]")
+_ = plt.ylabel("Count")
+
+plt.show()
